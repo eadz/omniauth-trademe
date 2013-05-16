d35136c168eca2a4810f7d2892951bb4c1548e4a
Ben Haines-Bellamy <benhaines@me.com>
2013-05-17 01:05:59 +1200

    Add TradmeSanbox class

--- lib/omniauth/strategies/trademe.rb
+++ lib/omniauth/strategies/trademe.rb
@@ -8,10 +8,10 @@ module OmniAuth
       option :name, 'trademe'
 
       option :client_options, {
-        :site => 'https://secure.trademe.co.nz',
-        :request_token_path  => "/Oauth/RequestToken",
-        :access_token_path   => "/Oauth/AccessToken",
-        :authorize_path      => "/Oauth/Authorize"
+        :site => 'https://api.trademe.co.nz',
+        :request_token_path  => "https://secure.trademe.co.nz/Oauth/RequestToken",
+        :access_token_path   => "https://secure.trademe.co.nz/Oauth/AccessToken",
+        :authorize_path      => "https://secure.trademe.co.nz/Oauth/Authorize"
       }
 
       uid {
@@ -36,7 +36,7 @@ module OmniAuth
 
       # Return info gathered from the trademe API call
       def raw_info
-        @raw_info ||= MultiJson.decode(access_token.get('https://api.trademe.co.nz/v1/MyTradeMe/Summary.json').body)
+        @raw_info ||= MultiJson.decode(access_token.get('/v1/MyTradeMe/Summary.json').body)
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
       end
@@ -49,6 +49,15 @@ module OmniAuth
         options[:authorize_params] = {:perms => options[:scope]} if options[:scope]
         super
       end
+    end
+
+    class TrademeSandbox < Trademe
+      option :name, 'trademe_sandbox'
+
+      default_options[:client_options][:site] = 'https://api.tmsandbox.co.nz'
+      default_options[:client_options][:request_token_path] = 'https://secure.tmsandbox.co.nz/Oauth/RequestToken'
+      default_options[:client_options][:access_token_path] = 'https://secure.tmsandbox.co.nz/Oauth/AccessToken'
+      default_options[:client_options][:authorize_path] = 'https://secure.tmsandbox.co.nz/Oauth/Authorize'
     end
   end
 end
