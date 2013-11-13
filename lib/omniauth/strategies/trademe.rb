9ff0f22b998ce0b6a7cfc1d8412dc368ca566c0d
Ben Haines-Bellamy <benhaines@me.com>
2013-11-13 16:46:13 +1300

    Fix Trademe strategy, and change to new hash syntax

--- lib/omniauth/strategies/trademe.rb
+++ lib/omniauth/strategies/trademe.rb
@@ -8,10 +8,10 @@ module OmniAuth
       option :name, 'trademe'
 
       option :client_options, {
-        :site => 'https://api.trademe.co.nz',
-        :request_token_path  => "https://secure.trademe.co.nz/Oauth/RequestToken",
-        :access_token_path   => "https://secure.trademe.co.nz/Oauth/AccessToken",
-        :authorize_path      => "https://secure.trademe.co.nz/Oauth/Authorize"
+        site:                "https://api.trademe.co.nz",
+        request_token_url:   "https://secure.trademe.co.nz/Oauth/RequestToken",
+        access_token_url:    "https://secure.trademe.co.nz/Oauth/AccessToken",
+        authorize_url:       "https://secure.trademe.co.nz/Oauth/Authorize"
       }
 
       uid {
@@ -20,33 +20,35 @@ module OmniAuth
 
       info do
         {
-          :nickname => raw_info['Nickname'],
-          :email => raw_info['Email'],
-          :first_name => raw_info['FirstName'],
-          :last_name => raw_info['LastName'],
-          :name => [raw_info['FirstName'],raw_info['LastName']].join(' ')
+          nickname:     raw_info['Nickname'],
+          email:        raw_info['Email'],
+          first_name:   raw_info['FirstName'],
+          last_name:    raw_info['LastName'],
+          name:         [raw_info['FirstName'],raw_info['LastName']].join(' ')
         }
       end
 
       extra do
         {
-          :raw_info => raw_info
+          raw_info: raw_info
         }
       end
 
       # Return info gathered from the trademe API call
       def raw_info
-        @raw_info ||= MultiJson.decode(access_token.get('/v1/MyTradeMe/Summary.json').body)
-      rescue ::Errno::ETIMEDOUT
-        raise ::Timeout::Error
+        @raw_info ||= MultiJson.decode(access_token.get("#{site}/v1/MyTradeMe/Summary.json").body)
       end
 
       def user_info
         raw_info
       end
+      
+      def site
+        @options[:client_options][:site]
+      end
 
       def request_phase
-        options[:authorize_params] = {:perms => options[:scope]} if options[:scope]
+        options[:authorize_params] = {perms: options[:scope]} if options[:scope]
         super
       end
     end
@@ -55,9 +57,9 @@ module OmniAuth
       option :name, 'trademe_sandbox'
 
       default_options[:client_options][:site] = 'https://api.tmsandbox.co.nz'
-      default_options[:client_options][:request_token_path] = 'https://secure.tmsandbox.co.nz/Oauth/RequestToken'
-      default_options[:client_options][:access_token_path] = 'https://secure.tmsandbox.co.nz/Oauth/AccessToken'
-      default_options[:client_options][:authorize_path] = 'https://secure.tmsandbox.co.nz/Oauth/Authorize'
+      default_options[:client_options][:request_token_url] = 'https://secure.tmsandbox.co.nz/Oauth/RequestToken'
+      default_options[:client_options][:access_token_url] = 'https://secure.tmsandbox.co.nz/Oauth/AccessToken'
+      default_options[:client_options][:authorize_url] = 'https://secure.tmsandbox.co.nz/Oauth/Authorize'
     end
   end
 end
